2ffa677f64e81c2f9f76249d519aaf9b5fe84af2
Jay Gu <jagu@nvidia.com>
2025-12-09 19:02:49 -0800

    Adjust tolerance for matmul sample test
    Signed-off-by: Jay Gu <jagu@nvidia.com>

--- samples/MatMul.py
+++ samples/MatMul.py
@@ -298,7 +298,7 @@ if __name__ == "__main__":
     C_fp32_cutile = cutile_matmul(A_fp32, B_fp32)
     print(f"cuTile Output C shape: {C_fp32_cutile.shape}, dtype: {C_fp32_cutile.dtype}")
     if args.correctness_check:
-        torch.testing.assert_close(C_fp32_cutile, A_fp32 @ B_fp32, atol=1e-5, rtol=1e-3)
+        torch.testing.assert_close(C_fp32_cutile, A_fp32 @ B_fp32, atol=2e-5, rtol=1e-3)
         print("Correctness check passed")
     else:
         print("Correctness check disabled")
@@ -319,7 +319,7 @@ if __name__ == "__main__":
     C_non_mult_cutile = cutile_matmul(A_non_mult, B_non_mult)
     print(f"cuTile Output C shape: {C_non_mult_cutile.shape}, dtype: {C_non_mult_cutile.dtype}")
     if args.correctness_check:
-        torch.testing.assert_close(C_non_mult_cutile, A_non_mult @ B_non_mult, atol=1e-5, rtol=1e-3)
+        torch.testing.assert_close(C_non_mult_cutile, A_non_mult @ B_non_mult, atol=2e-5, rtol=1e-3)
         print("Correctness check passed")
     else:
         print("Correctness check disabled")
@@ -330,7 +330,7 @@ if __name__ == "__main__":
     print(f"cuTile Output C shape: {C_persistent_fp32_cutile.shape}, "
           f"dtype: {C_persistent_fp32_cutile.dtype}")
     if args.correctness_check:
-        torch.testing.assert_close(C_persistent_fp32_cutile, A_fp32 @ B_fp32, atol=1e-5, rtol=1e-3)
+        torch.testing.assert_close(C_persistent_fp32_cutile, A_fp32 @ B_fp32, atol=2e-5, rtol=1e-3)
         print("Correctness check passed")
     else:
         print("Correctness check disabled")
